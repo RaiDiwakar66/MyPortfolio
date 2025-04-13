cc8b94fc6555ea46aef975ced831ddbce4bcd5b2
umesh monger <Diwakarrai2020@gmail.com>
2025-04-12 23:35:30 -0400

    update certification Title

--- src/App.jsx
+++ src/App.jsx
@@ -201,16 +201,16 @@ export default function Portfolio() {
         <h2 className="text-3xl font-bold text-green-900 mb-10 text-center">Certifications 🏅</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 text-center">
           <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-xl transition">
-            <h3 className="text-xl font-semibold text-green-800">Embedded Systems - Arm</h3>
-            <p className="text-sm text-gray-700 mt-2">In-depth knowledge of embedded programming using Arm architecture.</p>
+            <h3 className="text-xl font-semibold text-green-800">Career Essential in Generative AI by Microsoft and LinkedIN</h3>
+            <p className="text-sm text-gray-700 mt-2">///</p>
           </div>
           <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-xl transition">
-            <h3 className="text-xl font-semibold text-green-800">STM32 Microcontroller</h3>
-            <p className="text-sm text-gray-700 mt-2">Hands-on learning with Arm-based STM32 microcontrollers and real-world circuits.</p>
+            <h3 className="text-xl font-semibold text-green-800">AWS</h3>
+            <p className="text-sm text-gray-700 mt-2"></p>
           </div>
           <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-xl transition">
-            <h3 className="text-xl font-semibold text-green-800">FPGA & Digital Design</h3>
-            <p className="text-sm text-gray-700 mt-2">Training on Xilinx FPGA flow and RTL-based design for hardware systems.</p>
+            <h3 className="text-xl font-semibold text-green-800"></h3>
+            <p className="text-sm text-gray-700 mt-2">...</p>
           </div>
         </div>
       </section> 
