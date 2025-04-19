1c720896e9956501524ee7932be2b0fdec31a7cc
umesh monger <Diwakarrai2020@gmail.com>
2025-04-18 23:38:24 -0400

    Updated Certification Page

--- src/App.jsx
+++ src/App.jsx
@@ -207,6 +207,10 @@ export default function Portfolio() {
       <section id="certifications" className="min-h-screen py-20 px-6 relative z-10">
         <h2 className="text-3xl font-bold text-green-900 mb-10 text-center">Certifications 🏅</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 text-center">
+        <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-xl transition">
+            <h3 className="text-xl font-semibold text-green-800">Bachelor of Science in Software Engineering</h3>
+            <p className="text-sm text-gray-700 mt-2"></p>
+          </div>
           <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-xl transition">
             <img src={GenerativeAI} alt="Generative AI by" className="w-full h-48 object-contain mb-4"/>
             <h3 className="text-xl font-semibold text-green-800">Career Essential in Generative AI by Microsoft and LinkedIn</h3>
